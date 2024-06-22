6e015cd5ebb0a39a1ab242027716f7aa9abad17e
Nilam <nilam2002pawar@gmail.com>
2024-06-22 18:08:00 +0530

    states added for each data as well as error handaling added if  data is not available

--- src/App.js
+++ src/App.js
@@ -1,47 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Design from './designTask/task.js'; // Assuming Design is your array of tasks
 import DesignTask from './DesignTask.js';
 import Report from './Report.js';
-import develop from './designTask/developTask/develop.js'
+import develop from './designTask/developTask/develop.js';
 import projectReport from './designTask/developTask/projectreport/projrctReport.js';
+
 const App = () => {
+  const [uiux, setUiUx] = useState(Design);
+  const [backend, setBackend] = useState(develop);
+  const [report, setReport] = useState(projectReport);
+
   return (
     <div className='tasktracker'>
+      {/* UI/UX Design Tasks */}
       <div className='task'>
-     <center> <h2>UI/UX Design Task</h2></center>
-        {/* Mapping over the Design array to render tasks */}
-        {Design.map((item, index) => (
-          <>
-         
-          <DesignTask item={item} key={item}></DesignTask> 
-          </> 
-        ))}
+        <center><h2>UI/UX Design Task</h2></center>
+        {uiux && uiux.length > 0 ? (
+          uiux.map((item, index) => (
+            <DesignTask item={item} key={index}></DesignTask>
+          ))
+        ) : (
+         <center> <p>No UI/UX design tasks available.</p></center>
+        )}
       </div>
 
-
-
-       {/*  for develop tasks */}
-
-       <div className='task'> 
-       <center><h2> Backend Development Task</h2></center>
-        {/* Mapping over the develop array to render tasks */}
-        {develop.map((item, index) => (
-          <DesignTask item={item} key={item}></DesignTask> 
-        ))}
+      {/* Backend Development Tasks */}
+      <div className='task'>
+        <center><h2>Backend Development Task</h2></center>
+        {backend && backend.length > 0 ? (
+          backend.map((item, index) => (
+            <DesignTask item={item} key={index}></DesignTask>
+          ))
+        ) : (
+         <center><p>No backend development tasks available.</p></center> 
+        )}
       </div>
 
-
-
-
-       {/*  for project reports */}
-     
-       <div className='task'> 
-       <center><h2>Report Task</h2></center>
-        {/* Mapping over the Project report array to render tasks */}
-        {projectReport.map((item, index) => (
-          <Report item={item} key={item}></Report> 
-        ))}
+      {/* Project Report Tasks */}
+      <div className='task'>
+        <center><h2>Report Task</h2></center>
+        {report && report.length > 0 ? (
+          report.map((item, index) => (
+            <Report item={item} key={index}></Report>
+          ))
+        ) : (
+          <center><p>No report tasks available.</p></center>
+        )}
       </div>
     </div>
   );
